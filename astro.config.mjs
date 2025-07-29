9e853f97e518eef77f9cfe75a567c82ca0defd71
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-07-29 11:46:24 +0200

    feat: add wip content for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,45 +1,65 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import starlightSidebarTopics from 'starlight-sidebar-topics'
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify";
 
+import starlight from "@astrojs/starlight";
+import starlightSidebarTopics from "starlight-sidebar-topics";
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Crowous',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			plugins: [
+  adapter: netlify(),
+  integrations: [
+    starlight({
+      title: "Crowous",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/LiterateInk?q=Crowous",
+        },
+        {
+          icon: "discord",
+          label: "Discord",
+          href: "https://discord.gg/f5KNCnMWzB",
+        },
+      ],
+      plugins: [
         starlightSidebarTopics([
           {
             label: "Guides",
             link: "/guides/getting-started/installation",
             icon: "open-book",
             items: [
-              { label: "Getting Started", autogenerate: { directory: "guides/getting-started" } },
-              { label: "Examples", autogenerate: { directory: "guides/examples" } },
-            ]
-            // autogenerate: { directory: "guides" }
-            // items: [
-            //   { label: 'Example Guide', slug: 'guides/example' },
-            // ],
-          }
-        ])
+              {
+                label: "Getting Started",
+                autogenerate: { directory: "guides/getting-started" },
+              },
+              {
+                label: "Examples",
+                autogenerate: { directory: "guides/examples" },
+              },
+            ],
+          },
+          {
+            label: "References",
+            link: "/references",
+            icon: "document",
+            items: [
+              {
+                label: "Overview",
+                link: "/references",
+              },
+              {
+                label: "JS/TS",
+                link: "https://js.crowous.docs.literate.ink/",
+              },
+              {
+                label: "Rust",
+                link: "https://docs.rs/crowous/",
+              },
+            ],
+          },
+        ]),
       ],
-      // sidebar: [
-			// 	{
-			// 		label: 'Guides',
-			// 		items: [
-			// 			// Each item here is one entry in the navigation menu.
-			// 			{ label: 'Example Guide', slug: 'guides/example' },
-			// 		],
-			// 	},
-			// 	{
-			// 		label: 'Implementation',
-			// 		autogenerate: { directory: 'implementation' },
-			// 	},
-			// ],
-		}),
-	],
+    }),
+  ],
 });
